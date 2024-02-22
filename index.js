6c4929ddd62463670c92cdc40af6e0b4465e183d
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2024-02-22 22:15:55 +0530

    add first commit on this project

---
+++ index.js
@@ -0,0 +1,75 @@
+
+// detecting button keypress
+let allButtonsWithDrumClass = document.querySelectorAll(".drum").length;
+for(let i=0; i<allButtonsWithDrumClass; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+       
+    });
+}
+
+// detecting keyboard press
+document.addEventListener("keydown", function(event) {
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case "a":
+            let tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            let kickBass = new Audio("./sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector(".w");
+}
+
+// document.addEventListener("keydown", function(event) {
+//     console.log(event);
+// })
+
+
+// let audio = new Audio("./sounds/crash.mp3");
+// audio.play();
